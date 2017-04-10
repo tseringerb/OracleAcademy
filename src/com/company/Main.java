cfb6a015493e1240236f0e7c0ccf5f2f4dcaa5e8
Tseringer Boris <ceringerb@gmail.com>
2017-04-10 20:26:20 +0300

    Added iml file. Second commit.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(args);
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println(args);
+        }
     }
 }
